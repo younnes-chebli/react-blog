a60152cecc7263a454d6d4ea81fe29cd92328db5
Younnes Chebli <younnesc@gmail.com>
2023-10-30 12:39:02 +0100

    feat: react router hooks and links

--- src/App.js
+++ src/App.js
@@ -10,14 +10,46 @@ import { Route, Routes, useHistory } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 
 function App() {
+  const [posts, setPosts] = useState([
+    {
+      id: 1,
+      title: "My First Post",
+      datetime: "July 01, 2021 11:17:36 AM",
+      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
+    },
+    {
+      id: 2,
+      title: "My 2nd Post",
+      datetime: "July 01, 2021 11:17:36 AM",
+      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
+    },
+    {
+      id: 3,
+      title: "My 3rd Post",
+      datetime: "July 01, 2021 11:17:36 AM",
+      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
+    },
+    {
+      id: 4,
+      title: "My Fourth Post",
+      datetime: "July 01, 2021 11:17:36 AM",
+      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
+    }
+  ])
+  const [search, setSearch] = useState('')
+  const [searchResults, setSearchResults] = useState([])
+  const handleDelete = (id) => {
+
+  }
+
   return (
     <div className='App'>
-      <Header />
-      <Nav />
+      <Header title='React JS Blog' />
+      <Nav search={search} setSearch={setSearch} />
       <Routes>
-        <Route exact path='/' element={<Home />} />
+        <Route exact path='/' element={<Home posts={posts} />} />
         <Route exact path='/post' element={<NewPost />} />
-        <Route exact path='/post/:id' element={<PostPage />} />
+        <Route exact path='/post/:id' element={<PostPage posts={posts} handleDelete={handleDelete} />} />
         <Route exact path='/about' element={<About />} />
         <Route path='*' element={<Missing />} />
       </Routes>
